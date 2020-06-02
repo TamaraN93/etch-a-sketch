df74318266ae15c733a659c755bcbd7481b807cc
Tamara Nieves <tamaranieves93@gmail.com>
2020-06-03 00:04:55 +0200

    add grid generator with user input

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
+//SELECTORS
 const container = document.querySelector(".container");
-const rows = document.getElementsByClassName("row");
-const cells = document.getElementsByClassName("cell");
 const clear = document.querySelector(".clear");
 const square = document.getElementsByClassName("square");
+const generate = document.querySelector(".generate");
+
+//CUSTOM GRID
 
 //CREATE GRID
-
-function grid(num) {
-        container.getElementsByClassName.gridTemplateColumns = "repeat(16,1ft)";
-        for (let i = 1; i < num+1; i++) {
-            for (let j = 1; j < num+1; j++) {
-                const square = document.createElement("div");
-                square.classList.add("square");
-                square.style = "border: 1px solid black; background-color: white";
-                container.appendChild(square);
-            }
+function grid (size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style = "background-color: white; border: 1px solid black";
+            container.appendChild(square);
         }
     }
-
-grid(16);
-
-//HOVER EFFECT
-
-function hover() {
-    container.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "#ffa305";
-    })
+    
 }
 
+size = prompt("How many squares wide do you want your grid to be?");
+grid(size);
+
+
+
+        
+
+//HOVER EFFECT
+function hover() {
+    container.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor =  "lightcoral";
+    })
+}
 hover();
 
-
-
+//CLEAR GRID COLOR
 function clearGrid() {
     clear.addEventListener("click", (e) => {
         for (let i = 0; i < square.length; i++) {
@@ -39,8 +43,9 @@ function clearGrid() {
         }
     })
 }
-
 clearGrid();
+
+
 
 
 
